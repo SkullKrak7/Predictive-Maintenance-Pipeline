9c24f16bcfb108d1e3678574594a0d423afa121e
SkullKrak7 <sai.kagolanu@yahoo.com>
2025-03-15 00:38:31 +0000

    Handle missing values

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 
+#load data
 df = pd.read_csv("predictive_maintenance.csv")
 print("Initial Data Shape:", df.shape)
 print(df.head())
 
+#drop unnecessary columns
 df.drop(columns=['UDI', 'Product ID'], inplace=True)
 print("Data Shape after dropping useless columns:", df.shape)
+
+#handle missing values
+print("Missing Values Before Drop:")
+print(df.isna().sum())
+df.dropna(inplace=True)
+print("Missing Values After Drop:")
+print(df.isna().sum())
