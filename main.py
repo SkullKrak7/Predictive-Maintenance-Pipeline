d48598b4893d668847894f7848fc0a3b00dd5e1b
SkullKrak7 <sai.kagolanu@yahoo.com>
2025-03-15 00:37:07 +0000

    drop unnecessary columns

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ import pickle
 df = pd.read_csv("predictive_maintenance.csv")
 print("Initial Data Shape:", df.shape)
 print(df.head())
+
+df.drop(columns=['UDI', 'Product ID'], inplace=True)
+print("Data Shape after dropping useless columns:", df.shape)
